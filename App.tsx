7cee6540365bc3e5f6f6994f1e3998b32db9be5f
Banks Nussman <banks@nussman.us>
2020-12-24 15:26:45 -0500

    added sentry

--- App.tsx
+++ App.tsx
@@ -21,6 +21,13 @@ import { updatePushToken } from "./utils/Notifications";
 import socket, { getUpdatedUser } from './utils/Socket';
 import AsyncStorage from '@react-native-community/async-storage';
 import * as Updates from 'expo-updates';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://9bea69e2067f4e2a96e6c26627f97732@sentry.nussman.us/4',
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+});
 
 const Stack = createStackNavigator();
 
