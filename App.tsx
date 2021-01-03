fda674e360c64364cda222786fc27ae0a7a1b76d
Banks Nussman <banks@nussman.us>
2021-01-03 16:55:11 -0500

    clean App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React, { Component, ReactNode } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, StatusBar, Platform, AppState } from 'react-native';
+import { AppState } from 'react-native';
 import RegisterScreen from './routes/auth/Register';
 import LoginScreen from './routes/auth/Login';
 import ForgotPassword from './routes/auth/ForgotPassword';
@@ -15,40 +15,26 @@ import { default as beepTheme } from './utils/theme.json';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { ThemeContext } from './utils/ThemeContext';
 import { UserContext } from './utils/UserContext';
-import * as SplashScreen from 'expo-splash-screen';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
 import { updatePushToken } from "./utils/Notifications";
 import socket, { getUpdatedUser } from './utils/Socket';
 import AsyncStorage from '@react-native-community/async-storage';
-import * as Updates from 'expo-updates';
-import * as Sentry from 'sentry-expo';
-
-Sentry.init({
-    dsn: 'https://9bea69e2067f4e2a96e6c26627f97732@sentry.nussman.us/4',
-    enableInExpoDevelopment: true,
-    debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
-    enableAutoSessionTracking: true
-});
+import ThemedStatusBar from './utils/StatusBar';
+import { styles } from './utils/Styles';
+import { handleUpdateCheck } from './utils/Updates';
+import init from './utils/Init';
+import { isMobile, setSentryUserContext } from './utils/Sentry';
+import { User } from './types/Beep';
 
 const Stack = createStackNavigator();
-
-SplashScreen.preventAutoHideAsync()
-  .then(result => console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`))
-  .catch(console.warn); // it's good to explicitly catch and inspect any error
-
 let initialScreen: string;
-
-interface User {
-    token?: string;
-    isBeeping?: boolean;
-}
+init();
 
 interface Props {
     
 }
 
 interface State {
-    user: User;
+    user: User | any;
     theme: string;
 }
 
@@ -70,12 +56,7 @@ export default class App extends Component<Props, State> {
 
     setUser = (user: User): void => {
         this.setState({ user: user });
-        if ((Platform.OS == "ios" || Platform.OS == "android")) {
-            Sentry.Native.setUser({ ...user });
-        }
-        else {
-            Sentry.Browser.setUser({ ...user });
-        }
+        setSentryUserContext(user);
     }
 
     handleAppStateChange = (nextAppState: string): void => {
@@ -83,20 +64,9 @@ export default class App extends Component<Props, State> {
             socket.emit('getUser', this.state.user.token);
         }
     }
-
-    async handleUpdateCheck(): Promise<void> {
-        if (!__DEV__) {
-            const result = await Updates.checkForUpdateAsync();
-            console.log(result);
-            if (result.isAvailable) {
-                console.log("running Expo OTA update");
-                Updates.reloadAsync();
-            }
-        }
-    }
     
     async componentDidMount(): Promise<void> {
-        this.handleUpdateCheck();
+        handleUpdateCheck();
 
         AppState.addEventListener("change", this.handleAppStateChange);
 
@@ -109,7 +79,7 @@ export default class App extends Component<Props, State> {
             initialScreen = "Main";
             user = JSON.parse(storageData[0][1]);
             //If user is on a mobile device and user object has a token, sub them to notifications
-            if ((Platform.OS == "ios" || Platform.OS == "android") && user.token) {
+            if (isMobile && user.token) {
                 updatePushToken(user.token);
             }
 
@@ -118,12 +88,7 @@ export default class App extends Component<Props, State> {
                 socket.emit('getUser', user.token);
             }
 
-            if ((Platform.OS == "ios" || Platform.OS == "android")) {
-                Sentry.Native.setUser({ ...user });
-            }
-            else {
-                Sentry.Browser.setUser({ ...user });
-            }
+            setSentryUserContext(user);
         }
         else {
             initialScreen = "Login";
@@ -164,11 +129,7 @@ export default class App extends Component<Props, State> {
                     <IconRegistry icons={EvaIconsPack} />
                     <ApplicationProvider {...eva} theme={{ ...eva[this.state.theme], ...beepTheme }}>
                         <Layout style={styles.statusbar}>
-                            {Platform.OS == "ios" ?
-                                <StatusBar barStyle={(this.state.theme === 'light' ? 'dark' : 'light') + "-content"} />
-                                :
-                                <StatusBar translucent barStyle={(this.state.theme === 'light' ? 'dark' : 'light') + "-content"} backgroundColor={(this.state.theme === "dark") ? "#222b45" : "#ffffff"} />
-                            }
+                            <ThemedStatusBar theme={theme} />
                         </Layout>
                         <NavigationContainer>
                             <Stack.Navigator initialRouteName={initialScreen} screenOptions={{ headerShown: false }} >
@@ -186,9 +147,3 @@ export default class App extends Component<Props, State> {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    statusbar: {
-        paddingTop: getStatusBarHeight()
-    }
-});
--- utils/UserContext.ts
+++ utils/UserContext.ts
@@ -2,7 +2,8 @@ import React from 'react';
 import { User } from '../types/Beep';
 
 export interface UserContextData {
-    user: User;
+    user: any;
+    setUser: (user: User) => void;
 }
 
 export const UserContext = React.createContext<UserContextData | null>(null);
